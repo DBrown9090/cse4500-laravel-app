673b6a52dee3502b2bce65eea50478a95046891b
David Brown <007129090@coyote.csusb.edu>
2022-03-07 14:12:57 -0800

    Final assignment5 update

--- resources/views/calendar/show.blade.php
+++ resources/views/calendar/show.blade.php
@@ -10,6 +10,6 @@
   <h2>{{ $calendar->title; }}</h2>
   <div>Starts: <x-adminlte-input name="startdatetime" type="datetime-local" label="Start date and time" value="{{date('Y-m-d\Th:i',strtotime($calendar->startdatetime));}}" /></div>
   <div>Ends: <x-adminlte-input name="enddatetime" type="datetime-local" label="End date and time" value="{{date('Y-m-d\Th:i',strtotime($calendar->enddatetime));}}" /></div>
-  <p>Starts:{{date('Y-m-d\Th:i',strtotime($calendar->startdatetime));}}</p></div>
+  <div><p>Starts:{{date('Y-m-d\Th:i',strtotime($calendar->startdatetime));}}</p></div>
   <div><p>Ends:{{date('Y-m-d\Th:i',strtotime($calendar->enddatetime));}}</p></div>
 @stop
