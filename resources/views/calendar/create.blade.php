fca6dd04d67bfbc36d001b446f4dd03bd8985e5b
David Brown <007129090@coyote.csusb.edu>
2022-03-07 07:39:29 -0800

    oh my offset handling?

--- resources/views/calendar/create.blade.php
+++ resources/views/calendar/create.blade.php
@@ -10,8 +10,18 @@
 <form method="post" action="{{ route('calendar.store') }}" >
     @csrf
     <x-adminlte-input name="title" label="Title" />
+    <x-adminlte-input name="offset" id="hiddenoffsetinput" type="hidden" />
     <x-adminlte-input name="startdatetime" type="datetime-local" label="Start date and time" />
     <x-adminlte-input name="enddatetime" type="datetime-local" label="End date and time" />
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
+@stop
+
+@section('js')
+<script>
+$( document ).ready(function() {
+    let d = new Date();
+    document.getElementById("hiddenoffsetinput").value = (d.getTimezoneOffset() / 60);
+})
+</script>
 @stop
