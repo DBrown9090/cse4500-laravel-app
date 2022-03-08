16a147bd7069d88daf6d733705ca177fe1605935
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2022-03-07 18:12:58 -0800

    Update show.blade.php
    js handle timezone?

--- resources/views/calendar/show.blade.php
+++ resources/views/calendar/show.blade.php
@@ -8,8 +8,17 @@
 
 @section('content')
   <h2>{{ $calendar->title; }}</h2>
-  <div>Starts: <x-adminlte-input name="startdatetime" type="datetime-local" label="Start date and time" value="{{date('Y-m-d\Th:i',strtotime($calendar->startdatetime));}}" /></div>
-  <div>Ends: <x-adminlte-input name="enddatetime" type="datetime-local" label="End date and time" value="{{date('Y-m-d\Th:i',strtotime($calendar->enddatetime));}}" /></div>
-  <div><p>Starts:{{date('Y-m-d\Th:i',strtotime($calendar->startdatetime));}}</p></div>
-  <div><p>Ends:{{date('Y-m-d\Th:i',strtotime($calendar->enddatetime));}}</p></div>
+  <div>Starts: <x-adminlte-input name="startdatetime" id="startdatetime" type="datetime-local" label="Start date and time" value="" /></div>
+  <div>Ends: <x-adminlte-input name="enddatetime" id="enddatetime" type="datetime-local" label="End date and time" value="" /></div>
+@stop
+
+@section('js')
+<script>
+$( document ).ready(function() {
+    let start = new Date("{{date('Y-m-d\Th:i',strtotime($calendar->startdatetime));}}");
+    let end = new Date("{{date('Y-m-d\Th:i',strtotime($calendar->enddatetime));}}");
+    document.getElementById("startdatetime").value=start;
+    document.getElementById("enddatetime").value=end;
+})
+</script>
 @stop
